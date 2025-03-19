c9fdb3ac82388a548688e33b6d31b355e434502f
Gabriel <gabrielpecoraro@Mac.webpass.net>
2025-03-18 22:10:20 -0500

    Stuff

--- data_loader.py
+++ data_loader.py
@@ -3,22 +3,35 @@ from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 
+
+import torch
+
+# Check if GPU is available
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 # Specify the path to your PDF file
-
-loader = DirectoryLoader('./Dataset/', glob="./*.pdf", loader_cls=UnstructuredPDFLoader)
-
+print(device)
+print("a")
+loader = DirectoryLoader(
+    "./Dataset_bis/", glob="./*.pdf", loader_cls=UnstructuredPDFLoader
+)
+print("b")
+# documents = loader.load()
+loader_one_doc = DirectoryLoader(
+    "/1-EPC_17th_edition_2020_en.pdf",
+    glob="./*.pdf",
+    loader_cls=UnstructuredPDFLoader,
+)
 documents = loader.load()
-
-
+print("c")
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+print("d")
 chunks = text_splitter.split_documents(documents)
+print("e")
 
-
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
-chunk_vectors = [embeddings.embed(chunk) for chunk in chunks]
-
-
-
-
-
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+)
+print("f")
+chunk_vectors = embeddings.embed_documents([chunk.page_content for chunk in chunks])
+print("g")
